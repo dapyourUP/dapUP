f0f4bfba8fd0b1fa4736213996caa807ff523bcd
dapyourUP <111803304+dapyourUP@users.noreply.github.com>
2022-08-28 22:25:05 +0200

    Add files via upload

--- src/pages/ListUP.js
+++ src/pages/ListUP.js
@@ -6,7 +6,8 @@ import Items from '../components/Items';
 
 const ListUP = observer(() => {
   const { user } = useContext(Context);
-  const [np, setNp] = [useState(3)];
+  const [np, setNp] = useState(1);
+
   return (
     <div className="conteiner mx-auto max-w-[800px]  ">
       <div className="flex mx-auto justify-center text-white  from-pink-700 to-orange-300 hover:bg-gradient-to-bl  font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
@@ -18,12 +19,29 @@ const ListUP = observer(() => {
       </div>
 
       <div className="flex space-x-4 pt-5 justify-center">
-        <Items npList={np} />
+        <Items key={np} npList={np} />
       </div>
-      <div>
-        <p>{np}</p>
-        <button onClick={() => setNp(np + 1)}>Next|</button>
-        <button onClick={() => setNp(np - 1)}>Prev</button>
+
+      <div class="inline-flex rounded-md shadow-sm" role="group">
+        <button
+          className="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+          onClick={() => {
+            if (np > 1) {
+              setNp(np - 1);
+            }
+          }}
+        >
+          Prev
+        </button>
+        <p className="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+          {np}
+        </p>
+        <button
+          className="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+          onClick={() => setNp(np + 1)}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
